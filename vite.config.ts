9fd52c6addd76efdcfbd66b15746b29ec1d0ed55
Suyogya Luitel <lsuyogya@gmail.com>
2025-02-12 09:04:50 +0545

    init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+  },
+  plugins: [
+    react(),
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    tailwindcss(),
+  ],
+});
